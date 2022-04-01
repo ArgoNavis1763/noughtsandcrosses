bcd08f069b65be606e0a8d29334153d1a6d8a677
Carina Druce <trueblue546@gmail.com>
2022-04-01 18:13:51 +0100

    tests and dom working

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
-import  Board  from "./methods/board.js";
-const board = new Board()
+import Board from './methods/board.js';
+const board = new Board();
 
-document.addEventListener("DOMContentLoaded", () => {
-loadBoard(board.state);
+document.addEventListener('DOMContentLoaded', () => {
+  loadBoard(board.state);
 
-const container = document.querySelectorAll('.section')
-container.forEach((item) => {
+  const container = document.querySelectorAll('.section');
+  container.forEach((item) => {
     const squareIndex = item.dataset.index;
     const currentValue = board.state[squareIndex];
-    
+
     item.innerHTML = `<p>${currentValue}</p>`;
     item.addEventListener('click', () => {
-        const index = item.dataset.index;
-        const result = document.getElementById("result");
+      const index = item.dataset.index;
+      const result = document.getElementById('result');
 
-        try {
-            board.currentPlayerChange(index);
-            result.innerText = "";
-        } catch(error) {
-            result.innerText = "ERROR";
-        }
-       
-        loadBoard(board.state);
-        console.log(board.gameWon());
-        
-    })
-})
+      try {
+        board.currentPlayerChange(index);
+        result.innerText = '';
+      } catch (error) {
+        result.innerText = 'ERROR';
+      }
+
+      loadBoard(board.state);
+      console.log(board.gameWon());
+    });
+  });
 });
 
 const loadBoard = (boardState) => {
-    const container = document.querySelectorAll('.section');
-    container.forEach((item) => {
-        const squareIndex = item.dataset.index;
-        const currentValue = boardState[squareIndex];
-        item.innerHTML = `<p>${currentValue}</p>`;
-    });
-}
+  const container = document.querySelectorAll('.section');
+  container.forEach((item) => {
+    const squareIndex = item.dataset.index;
+    const currentValue = boardState[squareIndex];
+    item.innerHTML = `<p>${currentValue}</p>`;
+  });
+};
 
-const resetButton = document.getElementById("reset");
-resetButton.addEventListener("click", () => {
-    loadBoard(["", "", "", "", "", "", "", "", ""])
-})
-
-
-
+const resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', () => {
+  board.resetBoard();
+  loadBoard(board.state);
+});
--- methods/board.js
+++ methods/board.js
@@ -1,163 +1,126 @@
 export default class Board {
-    constructor(state = ["", "", "", "", "", "", "", "", ""], currentPlayer = "X") {
-      this.state = state;
-      this.currentPlayer = currentPlayer;
-    }
-    
+  constructor(state = ['', '', '', '', '', '', '', '', ''], currentPlayer = 'X', playerX, playerO) {
+    this.state = state;
+    this.currentPlayer = currentPlayer;
+    this.playerX = playerX;
+    this.playerO = playerO;
+  }
 
-    currentPlayerChange(index) {
-      this.addNewState(this.currentPlayer, index)
-      if (this.currentPlayer === "X") {
-      this.currentPlayer = "O"
-      } else {
-      this.currentPlayer = "X"
-     }
-  
-    }
-
-  
-    printBoard() {
-      let formattedString = "";
-      this.state.forEach((item, index) => {
-        if (item === "") {
-          formattedString += "   |";
-        } else {
-          formattedString += ` ${item} |`;
-        }
-        if (index === 2 || index === 5 || index === 8) {
-          formattedString = formattedString.slice(0, -1);
-          if (index !== 8) {
-            formattedString +=
-              "\n\u2015\u2015\u2015 \u2015\u2015\u2015 \u2015\u2015\u2015\n";
-          }
-        }
-      });
-      console.log("%c" + formattedString, "color: purple; font-size: 20px");
-    }
-  
-    isEmpty() {
-      let emptyArray = [];
-      this.state.forEach((item) => {
-        if (item === "X" || item === "O") {
-          emptyArray.push(item);
-        }
-      });
-      if (emptyArray.length > 0) {
-        return false;
-      } else {
-        return true;
-      }
-    }
-  
-    isFull() {
-      let fullArray = [];
-      this.state.forEach((item) => {
-        if (item === "X" || item === "O") {
-          fullArray.push(item);
-        }
-      });
-      if (fullArray.length === 9) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-    addNewState(newXO, index) {
-      if (newXO !== "X" && newXO !== "O") {
-        throw new Error("Invalid Character - please insert X or O");
-      } else if (this.state[index] !== "") {
-        throw new Error("This space is full");
-        
-      } else {
-        this.state[index] = newXO;
-        this.printBoard();
-        if (index > 8 || index < 0) {
-          throw new Error("Index Does Not Exist");
-        }
-        if (isNaN(index) === true) {
-          throw new Error("Index Does Not Exist");
-        }
-      }
-      return this.state;
-    }
-    availableIndex() {
-      let availableArray = [];
-      this.state.forEach((item, index) => {
-        if (item === "") {
-          availableArray.push(index);
-        }
-      })
-      return availableArray;
-    }
-    gameWon() {
-      let gameWon;
-      if (
-        this.state[0] === this.state[1] &&
-        this.state[1] === this.state[2] &&
-        this.state[2] !== ""
-      )
-        gameWon = { winner: this.state[0], direction: "horizontal" };
-      else if (
-        this.state[3] === this.state[4] &&
-        this.state[4] === this.state[5] &&
-        this.state[5] !== ""
-      )
-        gameWon = { winner: this.state[3], direction: "horizontal" };
-      else if (
-        this.state[6] === this.state[7] &&
-        this.state[7] === this.state[8] &&
-        this.state[8] !== ""
-      )
-        gameWon = { winner: this.state[6], direction: "horizontal" };
-      else if (
-        this.state[0] === this.state[3] &&
-        this.state[3] === this.state[6] &&
-        this.state[6] !== ""
-      )
-        gameWon = { winner: this.state[0], direction: "vertical" };
-      else if (
-        this.state[1] === this.state[4] &&
-        this.state[4] === this.state[7] &&
-        this.state[7] !== ""
-      )
-        gameWon = { winner: this.state[1], direction: "vertical" };
-      else if (
-        this.state[2] === this.state[5] &&
-        this.state[5] === this.state[8] &&
-        this.state[8] !== ""
-      )
-        gameWon = { winner: this.state[2], direction: "vertical" };
-      else if (
-        this.state[0] === this.state[4] &&
-        this.state[4] === this.state[8] &&
-        this.state[8] !== ""
-      )
-        gameWon = { winner: this.state[0], direction: "diagonal" };
-      else if (
-        this.state[2] === this.state[4] &&
-        this.state[4] === this.state[6] &&
-        this.state[6] !== ""
-      )
-        gameWon = { winner: this.state[2], direction: "diagonal" };
-      else if (this.isFull()) {
-        gameWon = { winner: "draw", direction: null };
-      } else {
-        gameWon = null;
-      }
-      return gameWon;
-      // const result = document.getElementById("result");
-      // if (gameWon === null) {
-      //   result.innerText = ""
-      //   } 
-      // else if (gameWon.winner === "draw" ) {
-      // result.innerText = "This game is a draw";
-      // } else {
-      //   result.innerText = `The winner of the game is ${gameWon.winner} and the direction is ${gameWon.direction}`;
-      // }
-     
+  currentPlayerChange(index) {
+    this.addNewState(this.currentPlayer, index);
+    if (this.currentPlayer === 'X') {
+      this.currentPlayer = 'O';
+    } else {
+      this.currentPlayer = 'X';
     }
   }
-  
- 
-  
 
+  printBoard() {
+    let formattedString = '';
+    this.state.forEach((item, index) => {
+      if (item === '') {
+        formattedString += '   |';
+      } else {
+        formattedString += ` ${item} |`;
+      }
+      if (index === 2 || index === 5 || index === 8) {
+        formattedString = formattedString.slice(0, -1);
+        if (index !== 8) {
+          formattedString += '\n\u2015\u2015\u2015 \u2015\u2015\u2015 \u2015\u2015\u2015\n';
+        }
+      }
+    });
+    console.log('%c' + formattedString, 'color: purple; font-size: 20px');
+    return formattedString;
+  }
+
+  isEmpty() {
+    let emptyArray = [];
+    this.state.forEach((item) => {
+      if (item === 'X' || item === 'O') {
+        emptyArray.push(item);
+      }
+    });
+    if (emptyArray.length > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  isFull() {
+    let fullArray = [];
+    this.state.forEach((item) => {
+      if (item === 'X' || item === 'O') {
+        fullArray.push(item);
+      }
+    });
+    if (fullArray.length === 9) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  addNewState(newXO, index) {
+    if (newXO !== 'X' && newXO !== 'O') {
+      throw new Error('Invalid Character - please insert X or O');
+    } else if (this.state[index] !== '') {
+      throw new Error('This space is full');
+    } else if (index > 8 || index < 0) {
+      throw new Error('Index Does Not Exist');
+    } else if (isNaN(index) === true) {
+      throw new Error('Index Does Not Exist');
+    } else {
+      this.state[index] = newXO;
+    }
+
+    return this.state;
+  }
+  availableIndex() {
+    let availableArray = [];
+    this.state.forEach((item, index) => {
+      if (item === '') {
+        availableArray.push(index);
+      }
+    });
+    return availableArray;
+  }
+  gameWon() {
+    let gameWon;
+    if (this.state[0] === this.state[1] && this.state[1] === this.state[2] && this.state[2] !== '')
+      gameWon = { winner: this.state[0], direction: 'horizontal' };
+    else if (this.state[3] === this.state[4] && this.state[4] === this.state[5] && this.state[5] !== '')
+      gameWon = { winner: this.state[3], direction: 'horizontal' };
+    else if (this.state[6] === this.state[7] && this.state[7] === this.state[8] && this.state[8] !== '')
+      gameWon = { winner: this.state[6], direction: 'horizontal' };
+    else if (this.state[0] === this.state[3] && this.state[3] === this.state[6] && this.state[6] !== '')
+      gameWon = { winner: this.state[0], direction: 'vertical' };
+    else if (this.state[1] === this.state[4] && this.state[4] === this.state[7] && this.state[7] !== '')
+      gameWon = { winner: this.state[1], direction: 'vertical' };
+    else if (this.state[2] === this.state[5] && this.state[5] === this.state[8] && this.state[8] !== '')
+      gameWon = { winner: this.state[2], direction: 'vertical' };
+    else if (this.state[0] === this.state[4] && this.state[4] === this.state[8] && this.state[8] !== '')
+      gameWon = { winner: this.state[0], direction: 'diagonal' };
+    else if (this.state[2] === this.state[4] && this.state[4] === this.state[6] && this.state[6] !== '')
+      gameWon = { winner: this.state[2], direction: 'diagonal' };
+    else if (this.isFull()) {
+      gameWon = { winner: 'draw', direction: null };
+    } else {
+      gameWon = null;
+    }
+    return gameWon;
+    // const result = document.getElementById("result");
+    // if (gameWon === null) {
+    //   result.innerText = ""
+    //   }
+    // else if (gameWon.winner === "draw" ) {
+    // result.innerText = "This game is a draw";
+    // } else {
+    //   result.innerText = `The winner of the game is ${gameWon.winner} and the direction is ${gameWon.direction}`;
+    // }
+  }
+
+  resetBoard() {
+    this.state = ['', '', '', '', '', '', '', '', ''];
+  }
+}
